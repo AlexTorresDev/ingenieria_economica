63390f5f84747f345bd55d0f5527e10fbd9c2631
NicolasRiveros <riveros.nicolas11@gmail>
2022-08-08 22:46:31 -0500

    cambios

---
+++ src/js/rentaperpetua.js
@@ -0,0 +1,22 @@
+import './main.js';
+
+const form = document.getElementById("RentaPerpetua");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const valorPresente = (form.Valor.value);
+    const interes = parseFloat(form.portentaje.value);
+
+    mostrarData(valorPresente, interes)
+
+});
+
+
+const mostrarData = (valor, tasa) => {
+    const tasaReal = tasa / 100;
+    //console.log(tasaReal)
+    const result = (valor / tasaReal);
+    // console.log(result,'este es el resultado')
+    document.getElementById("resultado").innerHTML = result;
+
+}
